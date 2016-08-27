ad91f6954d8a57f1c93f3137868def13feeb51ea
mikepetersonccv <MikePeterson@ccvonline.com>
2016-08-27 12:15:39 -0700

    update python

--- MikeTemperatureReader.py
+++ MikeTemperatureReader.py
@@ -3,14 +3,20 @@ import glob
 import time
 import sqlite3
 import datetime
+import RPi.GPIO as GPIO
+import time
+import sys
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+led_list = [26,21,20,16,12,25,24,23,18]
+GPIO.setup(led_list, GPIO.OUT)
 
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
-sensor1 = '28-031635fb46ff'
-sensor2 = '28-031636b23aff'
-sensor3 = '28-0416361444ff'
-sensor4 = '28-031635fff1ff'
+lower_attic_sensor = '28-031636b23aff'
+sensor_list = {1:'28-031635fb46ff',2:'28-0416361444ff',3:'28-031635fff1ff',4:'28-03163622ebff', 5:'28-031636b23aff', 6:'28-0416374e05ff'}
 
 base_dir = '/sys/bus/w1/devices/'
 
@@ -35,20 +41,71 @@ def read_temp(sensor):
         temp_f = temp_c * 9.0 / 5.0 + 32.0
         return temp_f
 
-def log_temperature(temp, sensor, dateTime):
-    print("%s %s %s" % (sensor, temp, str(dateTime)))
+def log_temperature(temp, sensorId, sensorAddress, dateTime):
+    print("%s %s" % (sensorAddress, temp, ))
     conn=sqlite3.connect('/home/pi/temperatureDatabase.db')
     curs=conn.cursor()
 
-    curs.execute("INSERT INTO temperatureLog (tdatetime, sensorId, temperature) values((?), ""?"", (?))", (dateTime, sensor,temp))
+    curs.execute("INSERT INTO temperatureLog (tdatetime, sensorId, temperature) values((?), ""?"", (?))", (dateTime, sensorId,temp))
     
     conn.commit()
     conn.close()
 
+    if sensorAddress == lower_attic_sensor:
+        GPIO.output(led_list,GPIO.LOW)
+        if temp >= 105:
+            GPIO.output(18,GPIO.HIGH)
+            GPIO.output(23,GPIO.HIGH)
+        elif temp >= 100:
+            GPIO.output(23,GPIO.HIGH)
+        elif temp >= 95:
+            GPIO.output(24,GPIO.HIGH)
+            GPIO.output(25,GPIO.HIGH)
+        elif temp >= 90:
+            GPIO.output(25,GPIO.HIGH)
+        elif temp >= 85:
+            GPIO.output(12,GPIO.HIGH)
+            GPIO.output(16,GPIO.HIGH)
+        elif temp >= 80:
+            GPIO.output(16,GPIO.HIGH)
+        elif temp >= 75:
+            GPIO.output(20,GPIO.HIGH)
+            GPIO.output(21,GPIO.HIGH)
+        else:
+            GPIO.output(21,GPIO.HIGH)
+        blinkcount = temp % 10
+        while blinkcount > 0:
+            if (blinkcount >= 5):
+                GPIO.output(18, GPIO.HIGH)
+                time.sleep(1)
+                blinkcount = blinkcount - 5
+            else:                
+                GPIO.output(18, GPIO.HIGH)
+                time.sleep(.1)
+                blinkcount = blinkcount - 1
+            GPIO.output(18, GPIO.LOW)
+            time.sleep(.2)
+            
+            
+
 while True:
-    currentDateTime = datetime.datetime.now()
-    log_temperature(read_temp(sensor1), sensor1, currentDateTime)
-    log_temperature(read_temp(sensor2), sensor2, currentDateTime)
-    log_temperature(read_temp(sensor3), sensor3, currentDateTime)
-    log_temperature(read_temp(sensor4), sensor4, currentDateTime)
-    time.sleep(30)
+    try:
+        GPIO.output(26,GPIO.HIGH)
+        currentDateTime = datetime.datetime.now()
+        print(str(currentDateTime))
+        for sensorId, sensorAddress in sensor_list.items():
+            log_temperature(read_temp(sensorAddress), sensorId, sensorAddress, currentDateTime)
+        print("__________________________________");
+        GPIO.output(26,GPIO.LOW)
+        time.sleep(30)
+    except:
+        print("error:", sys.exc_info())
+        GPIO.output(18, GPIO.HIGH)
+        time.sleep(3)
+        GPIO.output(18, GPIO.LOW)
+        time.sleep(1)
+        GPIO.output(18, GPIO.HIGH)
+        time.sleep(3)
+        GPIO.output(18, GPIO.LOW)
+        
+
